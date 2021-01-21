46b868d8061b31466b4b6ad85a9e98604d7d4ad9
Krithika S Udupa <41021851+Kiittyka@users.noreply.github.com>
2021-01-21 14:12:22 +0530

    Added AzFunction directory

---
+++ AzFunction/Functions/checkRecordForLogin.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Logging;
+
+namespace Company.Function
+{
+    public static class checkRecordForLogin
+    {
+        [FunctionName("checkRecordForLogin")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            string sqlResult;
+            string responseMessage = "This HTTP triggered function executed successfully";
+            string connectionString = "Server=tcp:uc1.database.windows.net,1433;Initial Catalog=RoasterDb;Persist Security Info=False;User ID=adminuser;Password=Admin@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            // Using the connection string to open a connection
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    // Opening a connection
+                    connection.Open();
+
+                    // Defining the registration form details
+                    var userName = req.Query["userName"];
+                    var password = req.Query["password"];
+
+                    // Prepare the SQL Query
+                    var query = $"SELECT [RoleId] FROM [UserDetails] WHERE [UserName] = '{userName}' AND [Pass] = '{password}'";
+                    // Prepare the SQL command and execute query
+                    SqlCommand command = new SqlCommand(query, connection);
+
+                    // Open the connection, execute and close connection
+                    if (command.Connection.State == System.Data.ConnectionState.Open)
+                    {
+                        command.Connection.Close();
+                    }
+                    command.Connection.Open();
+                    int result = (int)(command.ExecuteScalar());
+                    sqlResult = String.Format("{0}", result);
+                    return new OkObjectResult(sqlResult);
+                }
+            }
+            catch (Exception e)
+            {
+                log.LogError(e.ToString());
+            }
+            return new OkObjectResult(responseMessage);
+        }
+
+    }
+}
