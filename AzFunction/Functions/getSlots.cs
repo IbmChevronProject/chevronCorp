7e1df76856598b6c75fe5dd2807665a089c975a2
justinchev <77729736+justinchev@users.noreply.github.com>
2021-02-08 23:46:32 +0530

    New Function added

---
+++ AzFunction/Functions/getSlots.cs
@@ -0,0 +1,62 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Company.Function
+{
+    public static class getSlots
+    {
+        [FunctionName("getSlots")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            List<string> sqlResult = new List<string>();
+            string responseMessage = "This HTTP triggered function executed successfully";
+            string connectionString = "Server=tcp:uc1.database.windows.net,1433;Initial Catalog=RoasterDb;Persist Security Info=False;User ID=adminuser;Password=Admin@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+            // Using the connection string to open a connection
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    // Opening a connection
+                    connection.Open();
+
+                    
+
+                    // Prepare the SQL Query
+                    var query = $"SELECT [SlotId],[SlotName] FROM [Slots] order  by SlotId";
+                    // Prepare the SQL command and execute query
+                    SqlCommand command = new SqlCommand(query, connection);
+
+                    // Open the connection, execute and close connection
+                    if (command.Connection.State == System.Data.ConnectionState.Open)
+                    {
+                        command.Connection.Close();
+                    }
+                    command.Connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        sqlResult.Add(String.Format("{0},{1}", reader[0],reader[1]));
+                    }
+                    return new OkObjectResult(sqlResult);
+                }
+            }
+            catch (Exception e)
+            {
+                log.LogError(e.ToString());
+            }
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
